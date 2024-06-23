089c4eccefd2d8e670cf069bd882f86eeb60c702
zeevg <zeevg@wix.com>
2024-06-23 17:41:34 +0300

    add search

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,26 +1,47 @@
 import {useEffect, useState} from 'react'
-import * as styles from './Countries..module.css'
+import * as styles from './Countries.module.css'
+import {useDebounce} from "use-debounce";
 
 
 function App() {
     const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState('');
+    const [searchDebounced] = useDebounce(search, 600);
 
-    const fetchDate = async () => {
-        const url = 'https://restcountries.com/v3.1/all';
+
+    const fetchDate = async (search = '') => {
+        let url;
+        if (search) {
+            url = `https://restcountries.com/v3.1/name/${search}`;
+        } else {
+            url = 'https://restcountries.com/v3.1/all';
+        }
+
         const res = await fetch(url).then(res => {
             return res.json()
         })
-        setCountries(res);
         console.log('res = ', res);
+
+        if (res.status === 404) {
+            setCountries([]);
+            return;
+        }
+
+        setCountries(res);
     }
 
     useEffect(() => {
-        fetchDate();
-    }, []);
+        console.log('z');
+        fetchDate(search);
+    }, [searchDebounced]);
 
 
     return (
         <>
+            <div>
+                <input type="text" placeholder="Search for country" onChange={(e) => setSearch(e.target.value)}/>
+            </div>
+            {countries.length === 0 && <h1>No countries found</h1>}
             <div className={styles.countries}>
                 {countries.map((country: any) => {
                     return (
